dc00d29a327fe66b0671005f3bcf896e98b8ed25
danangpurbojati <danangpurbojati@gmail.com>
2020-12-23 08:34:10 +0700

    finished all elements

--- src/App.js
+++ src/App.js
@@ -1,120 +1,248 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import menu from './assets/menu1.jpg';
+import Modal from '@material-ui/core/Modal';
+import Rating from '@material-ui/lab/Rating';
+import { withStyles } from '@material-ui/core/styles';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import LocalGroceryStoreOutlinedIcon from '@material-ui/icons/LocalGroceryStoreOutlined';
+import ArrowForwardIosOutlinedIcon from '@material-ui/icons/ArrowForwardIosOutlined';
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
+import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
 
-const dates = [
-  {id:1, date:10, day:"sen"},
-  {id:2, date:11, day:"sel"},
-  {id:3, date:12, day:"rab"},
-  {id:4, date:13, day:"kam"},
-  {id:5, date:14, day:"jum"},
-  {id:6, date:15, day:"sab"},
-  {id:7, date:16, day:"min"},
-  {id:8, date:17, day:"sen"},
-]
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+import moment from 'moment';
+import foods from './DummyFood';
+
 
 function App() {
+  const [meals, setMeal] = useState([]);
+  const [showSearch, setShowSearch] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [dinnerActive, setdinnerActive] = useState(true)
+  const [showDinner, setShowDinner] = useState(true);
+  const [scrollPos, setScrollPos] = useState(0);
+  const [dates, setDate] = useState([]);
+  const [prices, setPrice] = useState(0);
+  const [items, setItem] = useState(0);
+  const [datePick, setDatePick] = useState(new Date());
+
+  const handleAddClick = (event) => {
+    let priceInteger = parseInt(event.target.value);
+    setPrice(prices + priceInteger);
+    setItem(items + 1);
+  }
+  
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    // setShowDinner((scrollPos > currentScrollPos && scrollPos - currentScrollPos > 30) || currentScrollPos < 10);
+    setShowDinner(scrollPos > currentScrollPos);
+    setScrollPos(currentScrollPos)
+  }
+
+  const getDatesBetweenDates = (startDate, endDate) => {
+    let dates = [];
+    const theDate = new Date(startDate);
+    while (theDate < endDate) {
+      dates = [...dates, new Date(theDate)];
+      theDate.setDate(theDate.getDate() + 1);
+    }
+    return dates;
+  }
+
+  const onChangeSearch = event => {
+    if(event.target.value.length > 2){
+      setShowSearch(true);
+    }else{
+      setShowSearch(false);
+    }
+  }
+
+  const setDatePickHandler = (tes) => {
+    setDatePick(tes);
+  }
+
+  const numberWithCommas = number => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  useEffect(() => {
+    setMeal(foods)
+  }, [meals])
+
+  useEffect(() => {
+    let today = new Date();
+    let twoWeeks = new Date(today);
+    twoWeeks.setDate(twoWeeks.getDate() + 14);
+    setDate(getDatesBetweenDates(today, twoWeeks));
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  },[scrollPos, showDinner])
+
+  const StyledRating = withStyles({
+    iconFilled:{
+      color: "#f9423a"
+    }
+  })(Rating);
   return (
     <div>
 
-      
-      <div className="location">
-        <a className="back-button" href="/#">	&#8592;</a>
-        <button className="location-button">
-          <p>ALAMAT PENGANTARAN</p>
-          <h4>Tokopedia Tower <span>&#8595;</span></h4>
-        </button>
+      <div className="navbar">      
+        <div className="location">
+          <a className="back-button" href="/#">
+            <ArrowBackIcon />
+          </a>
+          <button onClick={() => setIsModalOpen(true)} className="location-button">
+            <p>ALAMAT PENGANTARAN</p>
+            <h4>Tokopedia Tower <span><ExpandMoreIcon /></span></h4>
+          </button>
+        </div>
+
+        <Modal className="modal-container" open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <div className="modal-content">
+              <button onClick={() => setIsModalOpen(false)} className="close-button">
+                <CloseOutlinedIcon />
+              </button>
+              <div className="modal-text">
+                <h3>Cek makanan yang tersedia di lokasi kamu</h3>
+                <div className="search-container">
+                  <RoomOutlinedIcon className="search-icon" />
+                  <input onChange={onChangeSearch} className="search-location" type="text"/>
+                </div>
+                {
+                  showSearch && (
+                    <div className="search-result">
+                      <div className="result-detail">
+                        <RoomOutlinedIcon />
+                        <div className="result-text">
+                          <p>Kulina</p>
+                          <p>jl abc gang def komplex zef</p>
+                        </div>
+                      </div>
+                      <div className="result-detail">
+                        <RoomOutlinedIcon />
+                        <div className="result-text">
+                          <p>Kulina</p>
+                          <p>jl abc gang def komplex zef</p>
+                        </div>
+                      </div>              
+                    </div>
+                  )
+                }
+                
+              </div>
+          </div>
+        </Modal> 
+
+        
+        <Splide
+          className="date-container"
+          options={ {
+            perPage: 7,
+            pagination: false,
+            arrows: false,
+            autowidth: true
+          } }
+        > 
+        {
+          dates.map(date => (
+            (moment(date).format('ddd') === "Sat" || moment(date).format('ddd') === "Sun") ? (
+              <SplideSlide key={date} >
+                <button disabled className="date">
+                  <p className="day">{moment(date).format('ddd')}</p>
+                  <p className="date-number">{moment(date).format('DD')}</p>
+                </button>
+              </SplideSlide>
+            ):(
+              <SplideSlide key={date} >
+                <button 
+                  style={(moment(datePick).format('DD') === moment(date).format('DD')) ? {backgroundColor: "#424749", color: "white"} : {backgroundColor: "white"}}
+                  onClick={() => setDatePickHandler(date)}
+                  className="date">
+                  <p className="day">{moment(date).format('ddd')}</p>
+                  <p className="date-number">{moment(date).format('DD')}</p>
+                </button>
+              </SplideSlide>
+            )
+            
+          ))
+        }
+                 
+        </Splide>
+
+
+        <div style={showDinner ? {display: "block"} : {display: "none"}} className="having-time">
+          <button onClick={() => setdinnerActive(!dinnerActive)} className="lunch" 
+            style={dinnerActive ? {backgroundColor: "#424749"} : {backgroundColor: "white"}}
+          >Lunch</button>
+          <button
+            style={dinnerActive ? {backgroundColor: "white"} : {backgroundColor: "#424749"} }
+           onClick={() => setdinnerActive(!dinnerActive)} className="dinner">Dinner</button>
+        </div>     
+
       </div>
 
 
-      <div className="date-container">
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-        <div className="date">
-          <p className="day">SEN</p>
-          <p className="date-number">10</p>
-        </div>
-      </div>
 
-
-
-
-      <div className="having-time">
-        <button className="lunch btn-active">Lunch</button>
-        <button className="dinner">Dinner</button>
-      </div>
-
-
-      
 
 
       <div className="App">
-        <h1 className="card-text-title">Kamis, 13 Maret 2019</h1>
-        <div className="card">
-          <img src={menu} alt="Avatar" />
-          <div className="card-text">
-            <p className="card-text-subtext">4.5</p>
-            <h3 className="card-text-title">Roasted Chicken With Scrambled Egg</h3>
-            <p className="card-text-subtext">by kulina Uptown Lunch</p>
-            <div className="price">
-              <p>Rp 35,000</p>
-              <button className="add-button">Add +</button>
-            </div> 
-          </div>
-        </div>
-        <div className="card">
-          <img src={menu} alt="Avatar" />
-          <div className="card-text">
-            <p className="card-text-subtext">4.5</p>
-            <h3 className="card-text-title">Roasted Chicken With Scrambled Egg</h3>
-            <p className="card-text-subtext">by kulina Uptown Lunch</p>
-            <div className="price">
-              <p>Rp 35,000</p>
-              <button className="add-button">Add +</button>
-            </div> 
-          </div>
-        </div>
+        <h1 className="card-text-title">{moment(datePick).format("ddd, DD MMMM YYYY")}</h1>
+
+        {
+          meals.map(meal => (
+            <div key={meal.id} className="card">
+              <img src={meal.imgSrc} alt="menu" />
+              <div className="card-text">
+                <div className="rating">
+                  <p className="card-text-subtext">{meal.rating}</p>
+                  <StyledRating precision={0.5} name="disabled" value={meal.rating} disabled />
+                </div>
+                <h3 className="card-text-title">{meal.name}</h3>
+                <p className="card-text-subtext">by kulina Uptown Lunch</p>
+                <div className="price">
+                  <p>{numberWithCommas(meal.price)}</p>
+                  <button value={meal.price} onClick={handleAddClick} className="add-button">
+                    ADD &#43;
+                  </button>
+                </div> 
+              </div>
+            </div>        
+
+          ))
+        }
+
       </div>
 
 
 
-
-      <div className="cart-container">
-        <div className="cart">
-          <div className="cart-text">
-            <p className="cart-price">
-              <span>5 Items</span>
-              <span>|</span>
-              <span>Rp 125,000</span>
-            </p>
-            <p className="cart-info">termasuk ongkos kirim</p>
+      {
+        (prices > 0) && (
+          <div className="cart-container">
+            <div className="cart">
+              <div className="cart-text">
+                <p className="cart-price">
+                  <span>{items} Items</span>
+                  <span>|</span>
+                  <span>Rp {numberWithCommas(prices)}</span>
+                </p>
+                <p className="cart-info">termasuk ongkos kirim</p>
+              </div>
+              <div className="cart-icon">
+                <LocalGroceryStoreOutlinedIcon />
+                <ArrowForwardIosOutlinedIcon />
+              </div>
+            </div>
           </div>
-          <div className="cart-icon">
-            <p>tes</p>
-          </div>
-        </div>
-      </div>
+        )
+      }
 
     </div>
   );
---
+++ src/DummyFood.js
@@ -0,0 +1,32 @@
+const foods = [
+    {
+        id: 1,
+        name: "Ayam Panggang",
+        imgSrc: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80",
+        price: 35000,
+        rating: 4
+    },
+    {
+        id: 2,
+        name: "Salad",
+        imgSrc: "https://images.unsplash.com/photo-1547496502-affa22d38842?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=584&q=80",
+        price: 40000,
+        rating: 4.5
+    },
+    {
+        id: 3,
+        name: "Daging Bakar",
+        imgSrc: "https://images.unsplash.com/photo-1553163147-622ab57be1c7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+        price: 50000,
+        rating: 3.5
+    },
+    {
+        id: 4,
+        name: "Mie Ayam",
+        imgSrc: "https://images.unsplash.com/photo-1526318896980-cf78c088247c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8&auto=format&fit=crop&w=500&q=60",
+        price: 30000,
+        rating: 5
+    }
+];
+
+export default foods;
